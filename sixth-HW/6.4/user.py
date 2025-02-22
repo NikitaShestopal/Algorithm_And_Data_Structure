68dff0fa593952cc89c8134dcd2000418449658e
Mykyta_Shestopal <snikita2206@gmail.com>
2025-02-22 16:25:45 +0200

    6.4 99%

--- sixth-HW/6.4/user.py
+++ sixth-HW/6.4/user.py
@@ -20,3 +20,29 @@ class HashTable:
         index = hash_function(author, title)
         if (author, title) in self.table[index]:
             self.table[index].remove((author, title))
+
+hash_table = HashTable()
+author_books = {}
+
+def init():
+    global hash_table, author_books
+    hash_table = HashTable()
+    author_books = {}
+
+def addBook(author, title):
+    hash_table.insert(author, title)
+    if author not in author_books:
+        author_books[author] = []
+    author_books[author].append(title)
+    author_books[author].sort()
+
+def find(author, title):
+    return hash_table.search(author, title)
+
+def delete(author, title):
+    hash_table.remove(author, title)
+    if author in author_books and title in author_books[author]:
+        author_books[author].remove(title)
+
+def findByAuthor(author):
+    return author_books.get(author, [])
