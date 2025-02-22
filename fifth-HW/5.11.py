e27bf2c597eeda5fa1096148a6678d16bdf55cd8
Mykyta_Shestopal <snikita2206@gmail.com>
2025-02-22 15:21:33 +0200

    5.11 88%

--- fifth-HW/5.11.py
+++ fifth-HW/5.11.py
@@ -1,20 +1,17 @@
 def max_trucks(R, L, B, X):
     left = 0
-    right = 0
     total_cost = 0
     max_trucks = 0
 
-    while right < R:
+    for right in range(R):
         median = X[(left + right) // 2]
         total_cost += abs(X[right] - median)
 
         while total_cost > B:
             total_cost -= abs(X[left] - median)
             left += 1
-            median = X[(left + right) // 2]
 
         max_trucks = max(max_trucks, right - left + 1)
-        right += 1
 
     return max_trucks
 
