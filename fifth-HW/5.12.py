e60cd68bf4e7f28782ebe140c2887f50ef46d81a
Mykyta_Shestopal <snikita2206@gmail.com>
2025-02-22 15:12:33 +0200

    5.12 "Аналіз розв'язку У вашій програмі у функції can_process_in_time ви знаходите суму оброблених деталей роботами типу A і B окремо, а потім берете мінімум з цих двох значень. Однак коректно буде перевірити сукупний результат обох типів роботів, адже деталі повинні пройти обробку обома типами роботів послідовно. Перевірте, чи правильно ви враховуєте логіку обробки деталей обома типами роботів по черзі.
    "

--- fifth-HW/5.12.py
+++ fifth-HW/5.12.py
@@ -1,17 +1,19 @@
-def can_process_in_time(T, N, Na, A, Nb, B):
+def can_process_in_time(T, N, A, B):
     total_A = sum(T // a for a in A)
     total_B = sum(T // b for b in B)
-    return min(total_A, total_B) >= N
+    return total_A >= N and total_B >= N
 
 def min_processing_time(N, Na, A, Nb, B):
-    left, right = 1, 10**9
+    left, right = 1, 10 ** 9
     while left <= right:
         mid = (left + right) // 2
-        if can_process_in_time(mid, N, Na, A, Nb, B):
+        if can_process_in_time(mid, N, A, B):
             right = mid - 1
         else:
             left = mid + 1
-    return left
+
+    return left + 1 if can_process_in_time(left, N, A, B) else left
+
 
 N = int(input())
 Na = int(input())
@@ -20,3 +22,4 @@ Nb = int(input())
 B = list(map(int, input().split()))
 
 print(min_processing_time(N, Na, A, Nb, B))
+
