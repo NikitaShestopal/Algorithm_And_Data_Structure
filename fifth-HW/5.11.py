7aa40be5975e9aad49e57cc36dd627e2f53a64f5
Mykyta_Shestopal <snikita2206@gmail.com>
2025-02-22 15:33:26 +0200

    5.11 completely screwed up the program because of the prefix

--- fifth-HW/5.11.py
+++ fifth-HW/5.11.py
@@ -1,19 +1,33 @@
 def max_trucks(R, L, B, X):
+    prefix_sum = [0] * (R + 1)
+
+    for i in range(R):
+        prefix_sum[i + 1] = prefix_sum[i] + X[i]
+
     left = 0
-    total_cost = 0
     max_trucks = 0
 
     for right in range(R):
-        median = X[(left + right) // 2]
-        total_cost += abs(X[right] - median)
+        median_idx = (left + right) // 2
+        median = X[median_idx]
+
+        left_cost = median * (median_idx - left) - (prefix_sum[median_idx] - prefix_sum[left])
+        right_cost = (prefix_sum[right + 1] - prefix_sum[median_idx + 1]) - median * (right - median_idx)
+        total_cost = left_cost + right_cost
 
         while total_cost > B:
-            total_cost -= abs(X[left] - median)
             left += 1
+            median_idx = (left + right) // 2
+            median = X[median_idx]
+
+            left_cost = median * (median_idx - left) - (prefix_sum[median_idx] - prefix_sum[left])
+            right_cost = (prefix_sum[right + 1] - prefix_sum[median_idx + 1]) - median * (right - median_idx)
+            total_cost = left_cost + right_cost
 
         max_trucks = max(max_trucks, right - left + 1)
 
     return max_trucks
+
 
 R, L, B = map(int, input().split())
 X = [int(input()) for _ in range(R)]
